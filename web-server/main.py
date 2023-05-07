1793e2cbf465f727e2a2423e19284ae061ee3b59
David Henao <dhhenaom.10@gmail.com>
2023-05-07 14:17:44 -0500

    web server cambios

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,30 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI() # SE CREA UNA INSTANCIA DE ESTA APLICACIÓN
+ 
+@app.get('/') # ES UN DECORADOR QUE SIRVE INIDICAR QUE CON ESA DIRECCIÓN SE PUEDE ACCEDER AHI
+def get_list():
+    return [1,2,3,4]
+
+@app.get('/contact/2',response_class=HTMLResponse) # ES UN DECORADOR QUE SIRVE 
+async def get_list():
+    return """
+        <html>
+        <h1>Hola soy una página<\h1>
+        <b>Hola soy un parrafo</b>
+        </html>
+    """
+
+@app.get('/prueba',response_class=HTMLResponse) # ES UN DECORADOR QUE SIRVE 
+async def get_list():
+    return """
+        <html>
+        <h1>Hola soy una página<\h1>
+        <b>Hola soy un parrafo</b>
+        </html>
+    """
 
 def run():
     store.get_categories()
